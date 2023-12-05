220171662188bc419c910280f01613015a849326
a.parfenova <Parfenovaa.aa@yandex.ru>
2023-12-05 11:55:30 +0300

    code fix

--- HospitalPatientStatus.py
+++ HospitalPatientStatus.py
@@ -8,17 +8,17 @@ class HospitalPatientStatus:
     def __init__(self, patients_db=None):
         self.patients_db = patients_db if patients_db else []
 
-    def patient_status_up(self, patient_id: int) -> None:
+    def _patient_status_up(self, patient_id: int) -> None:
         patient_index = patient_id - 1
         if self.patients_db[patient_index] == 3:
             result = input("Выписать пациента? (да/нет)")
             no_change_msg = f"Пациент остался в статусе '{PATIENT_STATUS[self.patients_db[patient_index]]}'"
-            self.patient_discharge(patient_index) if result in YES_COMMANDS else DialogWithUser.send_msg_to_user(no_change_msg)
+            self._patient_discharge(patient_index) if result in YES_COMMANDS else DialogWithUser.send_msg_to_user(no_change_msg)
         else:
             self.patients_db[patient_index] = self.patients_db[patient_index] + 1
             DialogWithUser.send_msg_to_user(self._create_status_changed_msg(patient_index))
 
-    def patient_status_down(self, patient_id: int) -> None:
+    def _patient_status_down(self, patient_id: int) -> None:
         patient_index = patient_id - 1
         if self.patients_db[patient_index] == 0:
             DialogWithUser.send_msg_to_user(self._patient_status_down_error())
@@ -40,12 +40,12 @@ class HospitalPatientStatus:
         if command in GET_STATUS_COMMANDS:
             DialogWithUser.send_msg_to_user(f"Статус пациента: '{PATIENT_STATUS[self.patients_db[patient_id - 1]]}'")
         elif command in STATUS_UP_COMMANDS:
-            self.patient_status_up(patient_id)
+            self._patient_status_up(patient_id)
         elif command in STATUS_DOWN_COMMANDS:
-            self.patient_status_down(patient_id)
+            self._patient_status_down(patient_id)
         elif command in DISCHARGE_COMMANDS:
-            self.patient_discharge(patient_id)
+            self._patient_discharge(patient_id)
 
-    def patient_discharge(self, patient_id: int) -> None:
+    def _patient_discharge(self, patient_id: int) -> None:
         self.patients_db.pop(patient_id - 1)
         DialogWithUser.send_msg_to_user(f"Пациент выписан из больницы")
--- tests.py
+++ tests.py
@@ -10,14 +10,14 @@ class TestsStatusDown:
     def test_status_down_complete(self):
         DialogWithUser.send_msg_to_user = MagicMock()
         hpa = HospitalPatientStatus([3, 1, 3,])
-        hpa.patient_status_down(patient_id=2)
+        hpa._patient_status_down(patient_id=2)
         assert hpa.patients_db == [3, 0, 3]
         DialogWithUser.send_msg_to_user.assert_called_with("Новый статус пациента: 'Тяжело болен'")
 
     def test_status_down_error(self):
         DialogWithUser.send_msg_to_user = MagicMock()
         hpa = HospitalPatientStatus([1, 0, 1,])
-        hpa.patient_status_down(patient_id=2)
+        hpa._patient_status_down(patient_id=2)
         assert hpa.patients_db == [1, 0, 1]
         DialogWithUser.send_msg_to_user.assert_called_with(
             "Ошибка. Нельзя понизить самый низкий статус (наши пациенты не умирают)"
@@ -26,10 +26,10 @@ class TestsStatusDown:
 
 class TestsCalculateStatistics:
     def test_get_statics_data_all_status(self):
-        hs = HospitalStatistics([2, 0, 1, 3, 0])
+        hs = HospitalStatistics([2, 0, 1, 3])
         raw_data = hs._get_calculate_statistics_data()
-        assert raw_data["patients_amount"] == 5
-        assert dict(raw_data["statistics"]) == {0: 2, 1: 1, 2: 1, 3: 1}
+        assert raw_data["patients_amount"] == 4
+        assert dict(raw_data["statistics"]) == {0: 1, 1: 1, 2: 1, 3: 1}
 
     def test_get_statics_data_different_status(self):
         hs = HospitalStatistics([3, 0, 3, 3])
@@ -38,12 +38,12 @@ class TestsCalculateStatistics:
         assert dict(raw_data["statistics"]) == {0: 1, 3: 3}
 
     def test_statistics_output_all_status(self):
-        expected_statistics = {0: 2, 1: 1, 2: 1, 3: 1}
-        expected_amount = 5
-        hs = HospitalStatistics([2, 0, 1, 3, 0])
+        expected_statistics = {0: 1, 1: 1, 2: 1, 3: 1}
+        expected_amount = 4
+        hs = HospitalStatistics([2, 0, 1, 3])
         actual_msg = hs._create_calculate_statistics_output(Counter(expected_statistics), expected_amount)
         assert actual_msg == (
-            "В больнице сейчас 5 чел., из них:\n\tв статусе 'Тяжело болен': 2 чел.\n\tв статусе 'Болен': 1 чел."
+            "В больнице сейчас 4 чел., из них:\n\tв статусе 'Тяжело болен': 1 чел.\n\tв статусе 'Болен': 1 чел."
             "\n\tв статусе 'Слегка болен': 1 чел.\n\tв статусе 'Готов к выписке': 1 чел."
         )
 
